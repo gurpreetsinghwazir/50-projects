0f516f63e1f02ab4a8f8466da04e69ff716cb553
gpswazir <gpswazir@gmail.com>
2024-06-29 00:18:45 +0530

    text not fitting

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const sections = document.querySelectorAll('.hello');
+   const sections = document.querySelectorAll('.hello');
     const text = document.querySelectorAll('h3');
 
-    sections.forEach(section => {
+    sections.forEach((section, index) => {
         section.addEventListener('click', function(event) {
             // Remove 'active' class from all other sections
-            sections.forEach(otherSection => {
-                if (otherSection !== section && otherSection.classList.contains('active')) {
+            sections.forEach((otherSection, otherIndex) => {
+                if (otherIndex !== index && otherSection.classList.contains('active')) {
                     otherSection.classList.remove('active');
-                   
-					
+                    text[otherIndex].classList.remove('active'); // Remove 'active' class from corresponding text
                 }
             });
 
             // Toggle 'active' class on the clicked section
-            section.classList.toggle('active');
-          
+           section.classList.toggle('active');
+            text[index].classList.toggle('active'); // Toggle 'active' class on corresponding text
         });
     });
 });
